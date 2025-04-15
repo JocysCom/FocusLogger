7db48c13b980d07dbb09a1b103b4fef93adc44cd
Evaldas Jocys <evaldas@jocys.com>
2025-04-15 10:25:47 +0100

    Update Comments.

--- FocusLogger/App.xaml.cs
+++ FocusLogger/App.xaml.cs
@@ -4,9 +4,6 @@ using System.Windows;
 namespace JocysCom.FocusLogger
 {
 
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
 	public partial class App : Application
 	{
 		public App()
--- FocusLogger/MainWindow.xaml.cs
+++ FocusLogger/MainWindow.xaml.cs
@@ -4,9 +4,6 @@ using System.Windows;
 
 namespace JocysCom.FocusLogger
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
